3b906b1699b5d6a7bab3f489393698c26b3c39a5
GildaTroya <35311721+GildaTroya@users.noreply.github.com>
2018-01-22 01:33:21 -0500

    Implementos GT

---
+++ ProyectoFinal/src/Proyecto/Final/Impl/ImplCategoria.java
@@ -0,0 +1,10 @@
+
+package Proyecto.Final.Impl;
+import Proyecto.Final.accesodatos.*;
+import Proyecto.Final.dao.*;
+import Proyecto.Final.rnegocio.entidades.*;
+import java.util.*;
+import java.sql.*;
+public class ImplCategoria {
+    
+}
---
+++ ProyectoFinal/src/Proyecto/Final/Impl/ImplDetalle_Ingreso.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto.Final.Impl;
+
+import Proyecto.Final.accesodatos.*;
+import Proyecto.Final.dao.*;
+import Proyecto.Final.rnegocio.entidades.*;
+import java.util.*;
+import java.sql.*;
+public class ImplDetalle_Ingreso {
+    
+}
