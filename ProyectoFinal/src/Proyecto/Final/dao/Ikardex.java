0361590e47ff16f2600168926cae636a6bf4c550
Usuario <Usuario@FI-LABC01-PC01>
2018-02-07 15:46:47 -0500

    Kardex

---
+++ ProyectoFinal/src/Proyecto/Final/dao/Ikardex.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto.Final.dao;
+import Proyecto.Final.rnegocio.entidades.*;
+import java.util.*;
+public interface Ikardex {
+    public int insertar (kardex kardex) throws Exception;
+    public int modificar(kardex kardex) throws Exception;
+    public int eliminar(kardex kardex) throws Exception;
+    public kardex obtener(int codigo) throws Exception;
+    public List<kardex> obtener() throws Exception;
+}
